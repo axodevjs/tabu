4709c2de03731d2180a4bbba9e60f60984f6b981
axodev <80778908+axodevjs@users.noreply.github.com>
2022-03-26 20:26:29 +0300

    Added a  "Add item" button to header

--- src/components/Molecules/Header/Mobile/Styled.jsx
+++ src/components/Molecules/Header/Mobile/Styled.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const StyledMobileHeader = styled.header`
   display: grid;
@@ -8,23 +8,36 @@ export const StyledMobileHeader = styled.header`
   justify-content: space-between;
   background: #ffffff;
   height: 80px;
-`
+`;
 
-export const Burger = styled.div``
+export const Burger = styled.div``;
 export const Logo = styled.div`
   display: grid;
   justify-self: center;
   min-width: 105px;
   align-items: center;
-`
+  cursor: pointer;
+`;
 
 export const UserBlock = styled.div`
   display: flex;
   justify-self: end;
-`
+`;
 
-export const Search = styled.div``
+export const Search = styled.div`
+  cursor: pointer;
+`;
 
 export const Cart = styled.div`
   margin-left: 20px;
-`
+  cursor: pointer;
+`;
+
+export const AddItem = styled.div`
+  margin-left: 24px;
+  cursor: pointer;
+`;
+
+export const LeftBox = styled.div`
+  display: flex;
+`;
